3fc7a48e0ea2ee719d968173ec88285f4c70f11e
YnBn <twb72743075@126.com>
2026-02-03 22:27:27 +0800

    amend setup.py

--- RFCI_RMIC/model/setup.py
+++ RFCI_RMIC/model/setup.py
@@ -1,20 +1,36 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from irap.Version import version
 
-setup(name='irap',
+setup(
+    name='irap',
     version=version,
     description='Intelligent RAAC-PSSM Protein Prediction Package',
     url='https://github.com/KingoftheNight/IRAP',
     author='Liang YC',
     author_email='1694822092@qq.com',
     license='BSD 2-Clause',
-    packages=['irap'],
-    install_requires=['pyecharts', 'numpy', 'scikit-learn', 'pandas', 'seaborn', 'ray'],
+    packages=find_packages(),
+    
+    # 明确指定要包含的数据文件夹
+    package_data={
+        'irap': [
+            'aaindexDB/*',
+            'bin/*',
+            'blastDB/*',
+            'raacDB/*',
+            'pdbaa',  
+        ],
+    },
+    
+    # 启用 package_data
+    include_package_data=True,
+    
+    install_requires=['pyecharts', 'numpy', 'scikit-learn', 'pandas', 'seaborn', 'ray', 'tqdm', 'xgboost'],
     entry_points={
         'console_scripts': [
-        'irap=irap.__main__:irap',
-            ]
-        },
+            'irap=irap.__main__:irap',
+        ]
+    },
     python_requires=">=3.6",
-    include_package_data=True,
-    zip_safe=True)
+    zip_safe=False,  # 由于包含数据文件，设为 False
+)
